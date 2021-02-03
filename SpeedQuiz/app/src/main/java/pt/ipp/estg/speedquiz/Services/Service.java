84ac8f348ad57d673d5710c8dd555ac20198fbab
Tatianaaas <33959107+Tatianaaas@users.noreply.github.com>
2021-02-03 17:06:05 +0000

    Update Service.java

--- SpeedQuiz/app/src/main/java/pt/ipp/estg/speedquiz/Services/Service.java
+++ SpeedQuiz/app/src/main/java/pt/ipp/estg/speedquiz/Services/Service.java
@@ -2,14 +2,22 @@ package pt.ipp.estg.speedquiz.Services;
 
 import java.util.List;
 
+import pt.ipp.estg.speedquiz.Models.Constructor;
 import pt.ipp.estg.speedquiz.Models.Driver;
+import pt.ipp.estg.speedquiz.Models.Results;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface Service {
-    @GET("{year}/drivers.json")
-    Call<List<Driver>> getDriversByYear(@Query("year") int year);
+    @GET("drivers.json")
+    Call<List<Driver>> getDriversByYear(@Query("year") int year, @Query("driverid") int name,@Query("round") int round);
+
+    @GET("constructors.json")
+    Call<List<Constructor>> getConstructorsByYear(@Query("year") int year,@Query("round") int round, @Query("constructorid") String name);
+
+    @GET("driverStandings.json")
+    Call<List<Results>> getResultsByYear(@Query("season") int year);
 
 }
